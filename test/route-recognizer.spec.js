0230e970ffffc79885eab14721f8535848948ada
Rob Eisenberg <rob@bluespire.com>
2014-12-09 23:01:59 -0500

    chore(*): set up the project structure and build
    This commit adds the initial source code, sets up the project
    structure, creates the gulp build file and configures karma.

---
+++ test/route-recognizer.spec.js
@@ -0,0 +1,8 @@
+import { RouteRecognizer } from '../lib/index';
+
+describe('route recognizer', () => {
+  it('should have some tests', () => {
+    var recognizer = new RouteRecognizer();
+    expect(recognizer).not.toBe(null);
+  });
+});
