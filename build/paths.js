abdc75f3933a751747e27ff8f0d7ba168c0798e1
Rob Eisenberg <rob@bluespire.com>
2016-06-13 14:50:33 -0400

    chore(all): update build, deps, d.ts gen, doc gen, lint

--- build/paths.js
+++ build/paths.js
@@ -7,7 +7,6 @@ var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 module.exports = {
   root: appRoot,
   source: appRoot + '**/*.js',
-  tests: 'test/**/*.js',
   html: appRoot + '**/*.html',
   style: 'styles/**/*.css',
   output: 'dist/',
@@ -15,4 +14,34 @@ module.exports = {
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/',
   packageName: pkg.name
+};var path = require('path');
+var fs = require('fs');
+
+// hide warning //
+var emitter = require('events');
+emitter.defaultMaxListeners = 20;
+
+var appRoot = 'src/';
+var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+
+var paths = {
+  root: appRoot,
+  source: appRoot + '**/*.js',
+  html: appRoot + '**/*.html',
+  style: 'styles/**/*.css',
+  output: 'dist/',
+  doc:'./doc',
+  e2eSpecsSrc: 'test/e2e/src/*.js',
+  e2eSpecsDist: 'test/e2e/dist/',
+  packageName: pkg.name,
+  ignore: [],
+  useTypeScriptForDTS: false,
+  importsToAdd: [],
+  sort: true
 };
+
+paths.files = [
+  paths.source
+];
+
+module.exports = paths;
